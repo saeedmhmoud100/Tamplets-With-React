a2fb4b8574e20e10aa68b51a5f064b09aea9f5af
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-10-19 20:30:12 +0200

    create the components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Header from "./Components/Home/Header/Header";
+import Work from "./Components/Home/Work/Work";
+import Skilles from "./Components/Home/Skills/Skilles";
+import Portfolio from "./Components/Home/Portfolio/Portfolio";
+import Profile from "./Components/Home/Profile/Profile";
+import SocialMedia from "./Components/Home/SocialMedia/SocialMedia";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-
-
-
   return (
     <div className="App">
-      App
-      
+      <Navbar />
+      <Header />
+      <Work />
+      <Portfolio />
+      <Skilles />
+      <Profile />
+      <SocialMedia />
+      <Footer />
     </div>
   );
 }
