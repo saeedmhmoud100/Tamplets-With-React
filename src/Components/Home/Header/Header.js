2e1880a370a87f3ee999b1308ec137d17b0e5fd0
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-10-21 09:44:40 +0200

    created header

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import "./header.css";
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className="home">
+      <div className="container">
+        <div className="home-information">
+          <h2 className="home-title margin-bottom">Home Title</h2>
+          <h4 className="home-info">Creative Director</h4>
+          <p className="home-desc">
+            Iam a professional <span>UX Designer</span> and Front-End Developer
+            creating modern and resposive designs to Web and Mobile. Let us work
+            together. Thank you.
+          </p>
+          <button className="home-btn">Let's Begin</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
+
 export default function Navbar() {
   return (
-    <div class="navbar">
-      <div class="container">
-        <div class="logo">
-          <h2 class="logo-text">Ultra Profile</h2>
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <h2 className="logo-text">Ultra Profile</h2>
         </div>
 
         <input type="checkbox" id="nav" />
         <label for="nav" />
 
-        <ul class="ul-list">
-          <li class="list-item">
+        <ul className="ul-list">
+          <li className="list-item">
             <NavLink to={"/"}>Home</NavLink>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <NavLink to={"#"}>Work</NavLink>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <NavLink to={"#"}>Portfolio</NavLink>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <NavLink to={"#"}>Resume</NavLink>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <NavLink to={"#"}>About</NavLink>
           </li>
-          <li class="list-item">
+          <li className="list-item">
             <NavLink to="/contact">Contact</NavLink>
           </li>
         </ul>
