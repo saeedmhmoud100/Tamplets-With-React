9f1ddec015e78537cca4dbe0b5e4cdfd8cec4ac3
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2022-10-21 18:41:38 +0200

    created contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Layouts/Navbar/Navbar";
+import Footer from "./Components/Layouts/Footer/Footer";
 import "./App.css";
 import Home from "./Components/Home/Home";
 import Contact from "./Components/Contact/Contact";
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import "./contact.css";
 export default function Contact() {
   return (
-    <div>
-      Contact
+    <div className="drop">
+      <div className="container">
+        <h2 className="drop-title">
+          <span>Drop </span>Me A line
+        </h2>
+        <form action="" onSubmit={e => e.preventDefault()}>
+          <div className="form-input">
+            <input type="text" placeholder="Your Name" />
+            <input type="email" placeholder="Your Email" />
+          </div>
+          <input type="text" className="sub" placeholder="Your Subject" />
+          <textarea cols="30" rows="10" placeholder="Your Message" />
+          <input type="submit" value="Send Message" />
+        </form>
+      </div>
     </div>
-  )
+  );
 }
